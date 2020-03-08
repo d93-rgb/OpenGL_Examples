fa1c05a2fccfd92d36ddc93654c18bccc7e70d41
d93-rgb <ggfa1993@gmail.com>
2020-03-08 01:12:53 +0100

    implemented constructors

--- OpenGL_Examples/core/include/camera.h
+++ OpenGL_Examples/core/include/camera.h
@@ -7,18 +7,20 @@ namespace ogl_examples
 class Camera
 {
 public:
+	Camera::Camera(glm::mat4 lookAt);
 
 protected:
-	glm::mat4 lookAt;
 	glm::mat4 cameraToWorld;
 	glm::mat4 worldToCamera;
+	glm::mat4 worldToRaster;
 };
 
 
 class OrthographicCamera : public Camera
 {
 public:
-	OrthographicCamera(glm::mat4 lookAt);
+	OrthographicCamera::OrthographicCamera(glm::mat4 lookAt,
+		float left, float right, float top, float bottom, float near, float far);
 
 protected:
 };
@@ -27,7 +29,9 @@ protected:
 class PerspectiveCamera : public Camera
 {
 public:
-	PerspectiveCamera(glm::mat4 lookAt);
+	PerspectiveCamera::PerspectiveCamera(glm::mat4 lookAt,
+		float fovy, float aspect_ratio, float near, float far);
+
 
 protected:
 };
--- OpenGL_Examples/core/src/camera.cpp
+++ OpenGL_Examples/core/src/camera.cpp
@@ -1,15 +1,28 @@
 #include "camera.h"
 
+#include "glm/gtc/matrix_transform.hpp"
+
 namespace ogl_examples
 {
-
-OrthographicCamera::OrthographicCamera(glm::mat4 lookAt)
+Camera::Camera(glm::mat4 lookAt) :
+	cameraToWorld(std::move(glm::inverse(lookAt))),
+	worldToCamera(std::move(lookAt))
 {
 
 }
 
-PerspectiveCamera::PerspectiveCamera(glm::mat4 lookAt)
+OrthographicCamera::OrthographicCamera(glm::mat4 lookAt, 
+	float left, float right, float top, float bottom, float near, float far) :
+	Camera(lookAt)
 {
+	worldToRaster = glm::ortho(left, right, bottom, top, near, far) * worldToCamera;
+}
+
+PerspectiveCamera::PerspectiveCamera(glm::mat4 lookAt,
+	float fovy, float aspect_ratio, float near, float far) :
+	Camera(lookAt)
+{
+	worldToRaster = glm::perspective(fovy, aspect_ratio, near, far) * worldToCamera;
 
 }
 
