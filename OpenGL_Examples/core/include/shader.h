cd57987484e8fe3c6d2832788b125945d32e8f39
d93-rgb <ggfa1993@gmail.com>
2020-03-17 16:55:17 +0100

    created shader class

---
+++ OpenGL_Examples/core/include/shader.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "opengl_examples.h"
+#include "GL/gl3w.h"
+
+namespace ogl_examples
+{
+
+class Shader
+{
+public:
+    Shader();
+
+    ~Shader();
+
+protected:
+
+};
+
+} // namespace ogl_examples
---
+++ OpenGL_Examples/core/src/shader.cpp
@@ -0,0 +1,12 @@
+#include "opengl_examples.h"
+#include "shader.h"
+
+namespace ogl_examples
+{
+
+Shader::Shader()
+{
+
+}
+
+} // namespace ogl_examples
